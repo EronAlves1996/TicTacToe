96ebe455664b4a9a671c69fe1ad7c9671f4cf0f1
EronAlves1996 <eron.alves@rocketmail.com>
2023-11-03 12:27:41 -0300

    feat: implement game validation logic

--- app/src/main/java/com/eronalves/tictactoe/ui/components/GameScreen.kt
+++ app/src/main/java/com/eronalves/tictactoe/ui/components/GameScreen.kt
@@ -1,5 +1,6 @@
 package com.eronalves.tictactoe.ui.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -20,22 +21,30 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.eronalves.tictactoe.R
 import com.eronalves.tictactoe.ui.components.viewmodel.CellStates
+import com.eronalves.tictactoe.ui.components.viewmodel.GameState
 import com.eronalves.tictactoe.ui.components.viewmodel.GlobalStateViewModel
 import com.eronalves.tictactoe.ui.components.viewmodel.PlayerTime
+import com.eronalves.tictactoe.ui.components.viewmodel.Winner
 import com.eronalves.tictactoe.ui.theme.player1Color
 import com.eronalves.tictactoe.ui.theme.player2Color
 
 data class CellState(val state: String, val color: Color)
 
 @Composable
-fun Cell(state: CellStates, onClick: () -> Unit, modifier: Modifier = Modifier) {
+fun Cell(
+    state: CellStates,
+    hasWinner: Boolean = false,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     Button(
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-        enabled = state == CellStates.Empty,
+        enabled = state == CellStates.Empty && !hasWinner,
         shape = RectangleShape,
         border = BorderStroke(
             1.dp,
@@ -61,6 +70,77 @@ fun Cell(state: CellStates, onClick: () -> Unit, modifier: Modifier = Modifier) 
     }
 }
 
+data class AgreggateDataGameState(
+    val playerTime: PlayerTime,
+    @StringRes val topTextContent: Int,
+    val bellowTextColor: Color,
+    val bellowTextContent: String
+)
+
+@Composable
+fun GameStateIndicator(globalUiState: GameState, modifier: Modifier = Modifier) {
+    val playerTime = globalUiState.playerTime
+    val player1Name = globalUiState.player1Name
+    val player2Name = globalUiState.player2Name
+    val agreggateState = when (globalUiState.winner) {
+        Winner.NoWinner -> (if (globalUiState.playerTime == PlayerTime.Player1) {
+            player1Name
+        } else {
+            player2Name
+        })?.let {
+            AgreggateDataGameState(
+                playerTime, R.string.player_time,
+                if (globalUiState.playerTime == PlayerTime.Player1) {
+                    MaterialTheme.colorScheme.player1Color
+                } else {
+                    MaterialTheme.colorScheme.player2Color
+                },
+                it
+            )
+        }
+
+        Winner.Match -> AgreggateDataGameState(
+            playerTime, R.string.its_a_match, if (globalUiState.playerTime == PlayerTime.Player1) {
+                MaterialTheme.colorScheme.player1Color
+            } else {
+                MaterialTheme.colorScheme.player2Color
+            }, ""
+        )
+
+        else -> (if (globalUiState.winner == Winner.Player1) {
+            globalUiState.player1Name
+        } else {
+            globalUiState.player2Name
+        })?.let {
+            AgreggateDataGameState(
+                playerTime, R.string.has_winner, if (globalUiState.winner == Winner.Player1) {
+                    MaterialTheme.colorScheme.player1Color
+                } else {
+                    MaterialTheme.colorScheme.player2Color
+                }, it
+            )
+        }
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        if (agreggateState != null) {
+            Text(
+                text = stringResource(id = agreggateState.topTextContent),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = agreggateState.bellowTextContent,
+                color = agreggateState.bellowTextColor,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+    }
+}
+
+
 @Composable
 fun GameScreen(
     viewModel: GlobalStateViewModel, modifier: Modifier = Modifier
@@ -72,27 +152,13 @@ fun GameScreen(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
+        GameStateIndicator(
+            globalUiState = globalUiState,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 20.dp)
-        ) {
-            Text(text = "Vez do Jogador", style = MaterialTheme.typography.titleLarge)
-            Text(
-                text = if (globalUiState.playerTime == PlayerTime.Player1) {
-                    globalUiState.player1Name!!
-                } else {
-                    globalUiState.player2Name!!
-                },
-                color = if (globalUiState.playerTime == PlayerTime.Player1) {
-                    MaterialTheme.colorScheme.player1Color
-                } else {
-                    MaterialTheme.colorScheme.player2Color
-                },
-                style = MaterialTheme.typography.titleLarge,
-            )
-        }
+        )
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
@@ -109,6 +175,7 @@ fun GameScreen(
                             onClick = {
                                 viewModel.makePlay(i, j)
                             },
+                            hasWinner = globalUiState.winner != Winner.NoWinner,
                             modifier = Modifier
                                 .weight(1.0F, fill = true)
                                 .fillMaxHeight()
--- app/src/main/java/com/eronalves/tictactoe/ui/components/viewmodel/GlobalStateViewModel.kt
+++ app/src/main/java/com/eronalves/tictactoe/ui/components/viewmodel/GlobalStateViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.util.Arrays
 
 enum class CellStates {
     Empty, X, O
@@ -13,13 +14,18 @@ enum class PlayerTime {
     Player1, Player2
 }
 
+enum class Winner {
+    Player1, Player2, Match, NoWinner
+}
+
 data class GameState(
     val player1Name: String? = null,
     val player2Name: String? = null,
     val isRobotEnabled: Boolean = false,
     val tableSize: Int = 3,
     val gameTable: Array<Array<CellStates>>? = null,
-    val playerTime: PlayerTime = PlayerTime.Player1
+    val playerTime: PlayerTime = PlayerTime.Player1,
+    val winner: Winner = Winner.NoWinner
 )
 
 
@@ -49,12 +55,15 @@ class GlobalStateViewModel : ViewModel() {
         _uiState.update {
             it?.gameTable?.get(x)?.set(y, associatePlayerToCellSymbol(it.playerTime))
             val table = it.gameTable
+            val hasWinner = checkIfGameEnded(table, it.tableSize)
 
             it.copy(
                 gameTable = table,
-                playerTime = swapPlaying(it.playerTime)
+                playerTime = swapPlaying(it.playerTime),
+                winner = hasWinner
             )
         }
+
     }
 
     private fun associatePlayerToCellSymbol(player: PlayerTime): CellStates {
@@ -69,5 +78,146 @@ class GlobalStateViewModel : ViewModel() {
             PlayerTime.Player1 -> PlayerTime.Player2
             else -> PlayerTime.Player1
         }
+    }
+
+    private fun checkIfGameEnded(table: Array<Array<CellStates>>?, tableSize: Int): Winner {
+        for (i in 0..tableSize - 1) {
+            val lineWinner = checkIfLineHasWinner(table, i)
+            if (lineWinner != Winner.NoWinner) return lineWinner
+            val columnWinner = checkIfColumnHasWinner(table, i)
+            if (columnWinner != Winner.NoWinner) return columnWinner
+        }
+        val diagonalWinner = checkIfDiagonalsHasWinner(table, tableSize)
+        if (diagonalWinner != Winner.NoWinner) return diagonalWinner
+        val isAMatch = checkIfMatch(table)
+        if (isAMatch != Winner.NoWinner) return isAMatch
+
+        return Winner.NoWinner
+    }
+
+    private fun checkIfLineHasWinner(table: Array<Array<CellStates>>?, lineNumber: Int): Winner {
+        val tableLine = table?.get(lineNumber)
+        var actualWinner = Winner.NoWinner
+
+        for (i in tableLine!!.indices) {
+            val cellState = tableLine[i]
+            if (cellState == CellStates.Empty) return Winner.NoWinner
+
+            if (actualWinner == Winner.NoWinner) {
+                actualWinner = assignWinner(cellState)
+            } else {
+                when (actualWinner) {
+                    Winner.Player1 -> if (cellState != CellStates.X) return Winner.NoWinner
+                    Winner.Player2 -> if (cellState != CellStates.O) return Winner.NoWinner
+                    else -> continue
+                }
+            }
+        }
+
+        return actualWinner
+    }
+
+    private fun assignWinner(cell: CellStates): Winner {
+        return when (cell) {
+            CellStates.X -> Winner.Player1
+            CellStates.O -> Winner.Player2
+            else -> Winner.NoWinner
+        }
+    }
+
+    private fun checkIfColumnHasWinner(
+        table: Array<Array<CellStates>>?,
+        columnNumber: Int
+    ): Winner {
+        var actualWinner = Winner.NoWinner
+
+        for (i in table!!.indices) {
+            val cellState = table[i][columnNumber]
+            if (cellState == CellStates.Empty) return Winner.NoWinner
+
+            if (actualWinner == Winner.NoWinner) {
+                actualWinner = assignWinner(cellState)
+            } else {
+                when (actualWinner) {
+                    Winner.Player1 -> if (cellState != CellStates.X) return Winner.NoWinner
+                    Winner.Player2 -> if (cellState != CellStates.O) return Winner.NoWinner
+                    else -> continue
+                }
+            }
+        }
+
+        return actualWinner
+    }
+
+    private fun checkIfDiagonalsHasWinner(
+        table: Array<Array<CellStates>>?,
+        tableSize: Int
+    ): Winner {
+        var actualWinner = Winner.NoWinner
+        val tableLength = tableSize - 1
+
+        for (i in 0..tableLength) {
+            val cellState = table!![i][i]
+
+            if (cellState == CellStates.Empty) {
+                actualWinner = Winner.NoWinner
+                break
+            }
+
+            if (actualWinner == Winner.NoWinner) {
+                actualWinner = assignWinner(cellState)
+            } else {
+                when (actualWinner) {
+                    Winner.Player1 -> if (cellState != CellStates.X) {
+                        actualWinner = Winner.NoWinner
+                        break
+                    }
+
+                    Winner.Player2 -> if (cellState != CellStates.O) {
+                        actualWinner = Winner.NoWinner
+                        break
+                    }
+
+                    else -> continue
+                }
+            }
+        }
+
+        if (actualWinner != Winner.NoWinner) return actualWinner
+
+        for (i in 0..tableLength) {
+            val cellState = table!![i][table[i].size - 1 - i]
+
+            if (cellState == CellStates.Empty) return Winner.NoWinner
+
+            if (actualWinner == Winner.NoWinner) {
+                actualWinner = assignWinner(cellState)
+            } else {
+                when (actualWinner) {
+                    Winner.Player1 -> if (cellState != CellStates.X) {
+                        actualWinner = Winner.NoWinner
+                        break
+                    }
+
+                    Winner.Player2 -> if (cellState != CellStates.O) {
+                        actualWinner = Winner.NoWinner
+                        break
+                    }
+
+                    else -> continue
+                }
+            }
+        }
+
+        return actualWinner
+    }
+
+    private fun checkIfMatch(table: Array<Array<CellStates>>?): Winner {
+        val totalOfEmpties = Arrays
+            .stream(table)
+            .map { line -> Arrays.stream(line).filter { item -> item == CellStates.Empty }.count() }
+            .reduce(0) { totalOfEmpties, emptyLineCount -> totalOfEmpties + emptyLineCount }
+        if (totalOfEmpties > 0) return Winner.NoWinner
+        return Winner.Match
     }
 }
